d89e5a02014b5da8ec6761035f3d20233a814d72
GGDog <1845732180@qq.com>
2023-03-07 17:49:18 +0800

    上传原来项目文件

---
+++ JS/FirstJavaScript.js
@@ -0,0 +1,11 @@
+function set(){
+    var a=2;
+    var b=4;
+    var str="xgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdgxgdg"
+
+    window.alert(a+b+str);
+}
+function gotToBaiDu(){
+    window.open("http://www.baidu.com");
+
+}
